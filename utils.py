8c0e637b600217ee40f4cd355887ca656addbe28
Boqi Chen <boqi.chen@mail.mcgill.ca>
2023-07-15 16:05:27 -0400

    evaluation scripts

---
+++ utils.py
@@ -0,0 +1,130 @@
+import networkx as nx
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+def convert_to_ancestor_graph(G):
+    '''Converts a (parent) tree to a graph with edges for all ancestor relations in the tree.'''
+    G_anc = nx.DiGraph()
+    for node in G.nodes():
+        for anc in nx.ancestors(G, node):
+            G_anc.add_edge(anc, node)
+    return G_anc
+
+
+def dataframe_to_ancestor_graph(df):
+    forest = []
+    for group in tqdm(list(set(df.group))):
+        forest.append(group_to_ancestor_graph(df, group))
+    return pd.concat(forest, ignore_index=True)
+
+
+def group_to_ancestor_graph(df, g):
+    df_tree=df[df.group==g]
+    graph = nx.DiGraph()
+    parents=df_tree['parent'].tolist()
+    children=df_tree['child'].tolist()
+    nodes = set(parents + children)
+    for node in nodes:
+        graph.add_node(node)
+    for i in range(len(parents)):
+        graph.add_edge(parents[i], children[i])
+
+    T = convert_to_ancestor_graph(graph)
+
+    df = nx.to_pandas_edgelist(T)
+    df['group']=g
+    df.columns=['parent','child','group']
+    df['compare']=df['parent']+df['child']+df['group'].astype(str)
+
+    return df
+    
+
+def maximum_likelihood(df_t, group):
+    def weight_filter(G):
+        edges_to_remove = []
+
+        for u, v, data in G.edges(data=True):
+           if data['weight'] <= np.log(0.5):
+               edges_to_remove.append((u, v))
+
+        G.remove_edges_from(edges_to_remove)
+        return G
+   
+    return post_process_predictions(df_t, group, weight_filter)
+
+
+def majority_voting(df_t, group, num_candidate=5):
+    threshold = int(num_candidate / 2) + 1
+
+    def weight_filter(G):
+        edges_to_remove = []
+
+        for u, v, data in G.edges(data=True):
+           if data['weight'] < threshold:
+               edges_to_remove.append((u, v))
+
+        G.remove_edges_from(edges_to_remove)
+        return G
+   
+    return post_process_predictions(df_t, group, weight_filter)
+
+
+def maximum_absorbance(df_t, group):
+    return post_process_predictions(df_t, group, nx.maximum_spanning_arborescence)
+
+
+def maximum_branching(df_t, group):
+    return post_process_predictions(df_t, group, nx.maximum_branching)
+
+
+def post_process_predictions(df_t, group, process_func):
+    df_tree=df_t[df_t.group==group]
+    graph = nx.DiGraph()
+    parents=df_tree['parent'].tolist()
+    children=df_tree['child'].tolist()
+    probabilities=df_tree['predict'].tolist()
+    nodes = set(parents + children)
+    for node in nodes:
+        graph.add_node(node)
+    for i in range(len(parents)):
+        graph.add_edge(parents[i], children[i], weight=probabilities[i])
+    
+    T = process_func(graph)
+    T = convert_to_ancestor_graph(T)
+
+    # convert back to pandas dataframe
+    df = nx.to_pandas_edgelist(T)
+    df['group'] = group
+
+    df.columns=['parent','child','group']
+    df['compare']=df['parent']+df['child']+df['group'].astype(str)
+    
+    return df 
+
+
+
+def evaluate_groups(df_actual, df_pred):
+    recall = []
+    precision = []
+    f1 = []
+
+    for group in tqdm(list(set(df_actual.group))):
+        group_actual = df_actual[df_actual.group == group]
+        group_pred = df_pred[df_pred.group == group]
+        group_common = pd.merge(group_actual, group_pred, on=['compare'], how='inner')
+
+        group_recall = len(group_common) / len(group_actual) if len(group_actual) > 0 else 0
+        group_precision = len(group_common) / len(group_pred) if len(group_pred) > 0 else 0
+
+        if group_recall + group_precision == 0:
+            group_f1 = 0
+        else:
+            group_f1 = 2 * (group_precision * group_recall) / (group_precision + group_recall)
+        
+        recall.append(group_recall)
+        precision.append(group_precision)
+        f1.append(group_f1)
+
+    return np.mean(recall), np.mean(precision), np.mean(f1)
