24bc01076ee5e556de227beb8959c00d6b86051e
Fandi YI <fandi.yi@mail.mcgill.ca>
2023-07-15 20:20:26 -0400

    add violation evaluation for all datasets & models

--- utils.py
+++ utils.py
@@ -46,8 +46,8 @@ def maximum_likelihood(df_t, group):
         edges_to_remove = []
 
         for u, v, data in G.edges(data=True):
-           if data['weight'] <= np.log(0.5):
-               edges_to_remove.append((u, v))
+            if data['weight'] <= np.log(0.5):
+                edges_to_remove.append((u, v))
 
         G.remove_edges_from(edges_to_remove)
         return G
@@ -62,8 +62,8 @@ def majority_voting(df_t, group, num_candidate=5):
         edges_to_remove = []
 
         for u, v, data in G.edges(data=True):
-           if data['weight'] < threshold:
-               edges_to_remove.append((u, v))
+            if data['weight'] < threshold:
+                edges_to_remove.append((u, v))
 
         G.remove_edges_from(edges_to_remove)
         return G
@@ -92,16 +92,17 @@ def post_process_predictions(df_t, group, process_func):
         graph.add_edge(parents[i], children[i], weight=probabilities[i])
     
     T = process_func(graph)
-    T = convert_to_ancestor_graph(T)
+#     T = convert_to_ancestor_graph(T)
 
     # convert back to pandas dataframe
+    
     df = nx.to_pandas_edgelist(T)
+    df=df[['source','target']]
     df['group'] = group
-
     df.columns=['parent','child','group']
     df['compare']=df['parent']+df['child']+df['group'].astype(str)
     
-    return df 
+    return df
 
 
 
@@ -128,3 +129,37 @@ def evaluate_groups(df_actual, df_pred):
         f1.append(group_f1)
 
     return np.mean(recall), np.mean(precision), np.mean(f1)
+
+
+
+def violation_val(res):
+    n_root=[]
+    no_root=[]
+    mul_parents=[]
+
+    for g in list(set(res.group)):
+        g_net=res[res.group==g]
+
+        G = nx.DiGraph()
+        G.add_edges_from(g_net[['parent','child']].values)
+        roots=  [n for n,d in G.in_degree() if d==0]
+        mul_parents.append([len(list(set(g_net[g_net.child==n].parent))) for n in set(g_net.child)])
+        n_root.append(len(roots))
+        no_root.append(1 if len(roots)==0 else 0)
+    avg_p=[np.mean(l) for l in mul_parents ]
+    count_mu_p=[len([i for i in l if i>1]) for l in mul_parents]
+    per_mu_p=[len([i for i in l if i>1])/len(l) for l in mul_parents]
+    df_viol=pd.DataFrame(list(zip(n_root,no_root,avg_p,count_mu_p,per_mu_p)),columns=['num_root','no_root','avg_parent','count_mult_parent','perc_mult_parent'])
+    return df_viol
+
+def avg_violation_val(dict_res):
+    res={'header':'count of roots'+"&"+'count of no root groups'+"&"+'avg number of parent'+'&'+'avg num of nodes with multiple parents'+'&'+'% nodes with multiple parents'}
+    for key, df in dict_res.items():
+        avg_num_root=round(np.mean(df.num_root),2)
+        avg_no_root= round(np.mean(df.no_root),2)
+        avg_parent=round(np.mean(df.avg_parent),2)
+        avg_count_mu_p=round(np.mean(df.count_mult_parent),2)
+        avg_per_mul_parent=round(np.mean(df.perc_mult_parent)*100,2)
+        res[key]=str(avg_num_root)+"&"+str(avg_no_root)+"&"+str(avg_parent)+'&'+str(avg_count_mu_p)+'&'+str(avg_per_mul_parent)
+
+    return res
